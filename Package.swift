3a3319c64431056b277ece9ea9780799c2ed4cf0
Kieran Brown <kb6627500@gmail.com>
2020-12-16 23:05:11 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,21 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "bez",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "bez",
             targets: ["bez"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "bez",
             dependencies: ["CGExtender"]),
